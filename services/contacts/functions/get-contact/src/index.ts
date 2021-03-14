abfc482cf9e8276370bba517b1becd90caa1cceb
kinshuk.jain <kinshuk.jain@klarna.com>
2021-03-14 19:00:03 +0100

    contact module

---
+++ services/contacts/functions/get-contact/src/index.ts
@@ -0,0 +1,94 @@
+import middy from '@middy/core'
+import { v4 as uuidv4 } from 'uuid'
+import logger from './logger'
+
+// should be first middleware
+const setCorrelationId = () => ({
+  before: (handler: any, next: middy.NextFunction) => {
+    if (!handler.event.headers['Correlation-Id']) {
+      const correlationId = uuidv4()
+      logger.setCorrelationId(correlationId)
+      handler.event.headers['Correlation-Id'] = correlationId
+    }
+    next()
+  },
+})
+
+// should be second middleware
+const errorHandler = () => ({
+  onError: (handler: any, next: middy.NextFunction) => {
+    let response = {}
+    if (handler.error.statusCode && handler.error.message) {
+      response = {
+        statusCode: handler.error.statusCode,
+        body: JSON.stringify({ error: handler.error.message }),
+      }
+    }
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Unkonwn error' }),
+    }
+    handler.response = {
+      ...response,
+      isBase64Encoded: false,
+      headers: {
+        'content-type': 'application/json',
+      },
+    }
+    logger.info(handler.response)
+    return next()
+  },
+})
+
+const HttpError = (status: number, message: string, body?: object): Error => {
+  const e: any = new Error(message)
+  e.statusCode = status
+  e.body = body
+  return e
+}
+
+const myHandler = async (event: any, context: any) => {
+  context.callbackWaitsForEmptyEventLoop = false
+
+  const requestStartTime = Date.now()
+  let response
+  try {
+    logger.info(event)
+    const authToken = event.headers['Authorization']
+
+    if (!authToken) {
+      throw HttpError(401, 'unauthorized')
+    }
+
+    response = {
+      isBase64Encoded: false,
+      statusCode: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        status: 'success',
+        message: 'successfully deleted user',
+      }),
+    }
+    return response
+  } catch (e) {
+    response = {
+      isBase64Encoded: false,
+      statusCode: e.statusCode || 500,
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        error: e.message || 'Something went wrong',
+      }),
+    }
+    return response
+  } finally {
+    logger.info({ ...response, response_time: Date.now() - requestStartTime })
+  }
+}
+
+export const handler = middy(myHandler)
+  .use(setCorrelationId())
+  .use(errorHandler())
---
+++ services/contacts/scripts/run.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+
+function display_usage {
+  echo "Usage: $0 <invoke|run> <delete-contact|get-contact|update-contact>"
+  exit 1
+}
+
+FUNCTION=$2
+COMMAND=$1
+
+if ! [[ "$FUNCTION" =~ ^(delete-contact|get-contact|update-contact)$ && "$COMMAND" =~ ^(invoke|run)$ ]]; then
+  display_usage
+fi
+
+sam --version; exit_code=$?
+
+if [ $exit_code -ne 0 ]; then
+  echo "Please install sam cli and run this script again"
+  echo "See how to install - https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html"
+  exit 1
+fi
+
+script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$script_path"
+cd "../functions/$FUNCTION" && yarn build
+cd ../../
+
+if [[ "$FUNCTION" =~ ^(update-contact)$ ]]; then
+  function_name="updateContactFunction"
+elif [[ "$FUNCTION" =~ ^(get-contact)$ ]]; then
+  function_name="getContactFunction"
+elif [[ "$FUNCTION" =~ ^(delete-contact)$ ]]; then
+  function_name="deleteContactFunction"
+else
+  display_usage
+fi
+
+sam build --template-file sam_template.yml $function_name; exit_code=$?
+if [ $exit_code -ne 0 ]; then
+  echo "build failed: ${exit_code}"
+  exit 1
+fi
+
+if [[ "$COMMAND" = "invoke" ]]; then
+  sam local invoke $function_name "${@:3}" --env-vars "env.json"
+elif [[ "$COMMAND" = "run" ]]; then
+  sam local start-api "${@:3}" --env-vars "env.json"
+fi
