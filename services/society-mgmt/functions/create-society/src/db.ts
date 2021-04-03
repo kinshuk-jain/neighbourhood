a7cd0164600296567f752f35e0c27e3442361699
kinshuk.jain <kinshuk.jain@klarna.com>
2021-04-03 02:25:26 +0200

    Add geocoding logic

--- services/society-mgmt/functions/create-society/src/db.ts
+++ services/society-mgmt/functions/create-society/src/db.ts
@@ -1,5 +1,6 @@
 import logger from './logger'
 import { v4 as uuidv4 } from 'uuid'
+import axios from 'axios'
 
 export const addSocietyRecord = async ({
   name,
@@ -8,22 +9,63 @@ export const addSocietyRecord = async ({
   address,
   society_type,
   show_directory,
-  latitude,
-  longitude,
 }: {
   [key: string]: any
 }) => {
   // use google geocoding api to get latitude and longitude for the address
   // if address is not found, we reject the request
-  const tableName = 'society'
+  let lat = '',
+    lng = ''
+
+  if (process.env.ENVIRONMENT !== 'development') {
+    const addressComponet = encodeURIComponent(
+      `${address.street_address}, ${address.city}, ${address.state}, ${address.postal_code}, ${address.country}`
+    )
+    const geocodeUri = `https://maps.googleapis.com/maps/api/geocode/json?language=en&key=${process.env.GOOGLE_GEOCODING_API_KEY}&address=${addressComponet}`
+
+    const { status, data = {} } = await axios.get(geocodeUri)
+
+    logger.info(data)
+
+    if (
+      status < 200 ||
+      status >= 300 ||
+      !data.status ||
+      (data.status !== 'OK' && data.status !== 'ZERO_RESULTS')
+    ) {
+      // api error
+      throw {
+        message: 'Internal service error: address could not be verified',
+        statusCode: 500,
+      }
+    } else if (data.status === 'ZERO_RESULTS') {
+      // invalid address
+      throw {
+        message: 'invalid address specified',
+        statusCode: 400,
+      }
+    } else if (data.results.length > 1) {
+      // ambiguous address
+      throw {
+        message:
+          'address specified is ambiguous. try making it more specific or contact us',
+        statusCode: 400,
+      }
+    }
+
+    // see https://developers.google.com/maps/documentation/geocoding/overview#StatusCodes
+    lat = data.results[1].geometry.location.lat
+    lng = data.results[1].geometry.location.lng
+  }
+
   logger.info({
     society_id: uuidv4(),
     name,
     billing_id: 123,
     admins,
     user_id,
-    latitude,
-    longitude,
+    latitude: lat,
+    longitude: lng,
     address,
     imp_contacts: [], // array of contact ids
     directory: [],
--- services/society-mgmt/functions/update-society/src/db.ts
+++ services/society-mgmt/functions/update-society/src/db.ts
@@ -1,4 +1,5 @@
 import logger from './logger'
+import axios from 'axios'
 
 export const updateSocietyPendingDeletionStatus = async (
   society_id: string,
@@ -17,9 +18,53 @@ export const updateSocietyName = async (society_id: string, name: string) => {
 }
 export const updateSocietyAddress = async (
   society_id: string,
-  address: object
+  address: { [key: string]: string }
 ) => {
-  logger.info({ society_id, address })
+  let lat = '',
+    lng = ''
+
+  if (process.env.ENVIRONMENT !== 'development') {
+    const addressComponet = encodeURIComponent(
+      `${address.street_address}, ${address.city}, ${address.state}, ${address.postal_code}, ${address.country}`
+    )
+    const geocodeUri = `https://maps.googleapis.com/maps/api/geocode/json?language=en&key=${process.env.GOOGLE_GEOCODING_API_KEY}&address=${addressComponet}`
+
+    const { status, data = {} } = await axios.get(geocodeUri)
+
+    logger.info(data)
+
+    if (
+      status < 200 ||
+      status >= 300 ||
+      !data.status ||
+      (data.status !== 'OK' && data.status !== 'ZERO_RESULTS')
+    ) {
+      // api error
+      throw {
+        message: 'Internal service error: address could not be verified',
+        statusCode: 500,
+      }
+    } else if (data.status === 'ZERO_RESULTS') {
+      // invalid address
+      throw {
+        message: 'invalid address specified',
+        statusCode: 400,
+      }
+    } else if (data.results.length > 1) {
+      // ambiguous address
+      throw {
+        message:
+          'address specified is ambiguous. try making it more specific or contact us',
+        statusCode: 400,
+      }
+    }
+
+    // see https://developers.google.com/maps/documentation/geocoding/overview#StatusCodes
+    lat = data.results[1].geometry.location.lat
+    lng = data.results[1].geometry.location.lng
+  }
+
+  logger.info({ society_id, address, latitude: lat, longitude: lng })
 }
 export const updateSocietyVerifiedStatus = async (
   society_id: string,
